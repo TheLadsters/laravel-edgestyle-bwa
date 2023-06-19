44846fcaa01f9e71e5b7b3833767ae3890243d9b
TheLadsters <jeraldmags@gmail.com>
2023-06-19 11:46:45 +0800

    Added login page, modified searchbar in chatspace page

--- react/src/components/SearchBar.jsx
+++ react/src/components/SearchBar.jsx
@@ -2,30 +2,31 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchIcon from '@mui/icons-material/Search';
+import IconButton from '@mui/joy/IconButton';
+import Input from '@mui/joy/Input';
 
 export default function SearchBar() {
     const styles = {
-        inputGroup:  {
-          width: '100%',
-          height: '4%',
-          marginBottom: '20px', 
-          marginTop: '20px',
-          size: '10px',
-          border: 'none',
-          borderRadius: '20px',
-        },
         searchBar: {
           borderRadius: '20px',
+          marginBottom: '20px', 
+          marginTop: '20px',
         }
     }
 
 
 
   return (
-    <div style={styles.inputGroup} className="input-group">
-      <input placeholder="Search..."
-      type="text" className='form-control' style={styles.searchBar} />
-    </div>
+    <Input sx={styles.searchBar}
+      size="lg"
+      placeholder="Search..."
+      type="text"
+      endDecorator={
+        <IconButton color="white">
+          <SearchIcon color="action" />
+        </IconButton>
+      }
+  />
 
   )
 }
--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,9 +1,68 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/joy/IconButton';
+import Input from '@mui/joy/Input';
+import KeyRoundedIcon from '@mui/icons-material/KeyRounded';
+import VisibilityRoundedIcon from '@mui/icons-material/VisibilityRounded';
+import PersonIcon from '@mui/icons-material/Person';
 
 function Login() {
+const loginStyle = {
+    div: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh'
+  },
+
+  card: {
+    padding: '60px 20px',
+    width: '40%'
+  }
+}
   return (
-    <div>Login</div>
+    <div style={loginStyle.div}>
+      <Card sx={loginStyle.card}>
+        <Grid container spacing={2} 
+        direction={'column'} justifyContent={'center'}
+        alignContent={'center'}
+        >
+          <Grid item>
+            <h1>Edge Style Login</h1>
+          </Grid>
+
+          <Grid item>
+            <Input
+              size="lg"
+              startDecorator={<PersonIcon />}
+              placeholder="Username"
+              type="text"
+            />
+          </Grid>
+
+          <Grid item>
+            <Input
+              size="lg"
+              startDecorator={<KeyRoundedIcon />}
+              placeholder="Password"
+              type="password"
+              endDecorator={
+                <IconButton color="neutral">
+                  <VisibilityRoundedIcon />
+                </IconButton>
+              }
+            />
+          </Grid>
+
+          <Grid item>
+            <Button fullWidth={'100%'} variant="contained">Login</Button>
+          </Grid>
+        </Grid>
+      </Card>
+    </div>
   )
 }
 
